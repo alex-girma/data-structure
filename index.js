6a34322c81c2dceb18b01eaf8b264b4c0755be6f
alex-girma <56226961+alex-girma@users.noreply.github.com>
2022-06-30 22:16:21 +0200

    selection sort

--- index.js
+++ index.js
@@ -84,7 +84,18 @@ const bubbleSort = function (array) {
  *        - Go through array and put smallest value at the start of the array
  */
 
-
+const selectionSort = function (array) {
+	for (let i = 0; i < array.length; i++) {
+		let min = i;
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[min] > array[j]) {
+				min = j;
+			}
+		}
+		if (i !== min) [array[min], array[i]] = [array[i], array[min]];
+	}
+	return array;
+};
 
 // console.log(selectionSort(array));
 
